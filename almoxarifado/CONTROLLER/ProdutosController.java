1b78c6e93352949c18eb556f4c62aaa12d709655
elenylsoncbk <elenylsoncm@gmail.com>
2024-12-18 10:40:28 -0300

    Update ProdutosController.java

--- almoxarifado/CONTROLLER/ProdutosController.java
+++ almoxarifado/CONTROLLER/ProdutosController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/produtos")
+@RequestMapping("/almo-sys/produtos")
 public class ProdutosController {
 
     @Autowired
